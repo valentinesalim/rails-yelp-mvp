c95dfa335a9fca48ec8f7bc840936de53c2491ee
valentine salim <vp.mulia888@gmail.com>
2020-05-27 11:39:01 +0700

    checkpoint 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :restaurants do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+      collection do
+        get :top
+      end
+    end
 end
